7d703ceaf238ca3d1231c3e31098eefbf1175c1f
jupiter229 <smartwang1004@outlook.com>
2022-05-09 13:12:41 +0100

    Transfer assets

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+// import * as helmet from 'helmet';
+import helmet from 'helmet';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    // logger: ['error', 'warn'],
+  });
+  app.use(helmet());
   app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
+    // origin: '*',
+    // methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    // preflightContinue: false,
+    // optionsSuccessStatus: 204,
   });
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(new ValidationPipe());
